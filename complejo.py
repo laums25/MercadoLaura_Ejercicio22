29260fa81c4c48c2339be684692aa13cda315a13
laums25 <56890898+laums25@users.noreply.github.com>
2019-10-24 13:40:43 -0500

    Multiplicacion

---
+++ complejo.py
@@ -0,0 +1,35 @@
+import math 
+class Complejo():
+    def __init__(self, real, imaginario):
+        self.real = real
+        self.imaginario = imaginario
+        self.norma = math.sqrt(real*real + imaginario*imaginario)
+
+    def conjugado(self):
+        self.imaginario = -self.imaginario
+
+    def calcula_norma(self):
+        self.norma = math.sqrt(self.real**2 + self.imaginario**2)
+
+    def pow(self, n):
+        a = Complejo(self.real, self.imaginario)
+        self.calcula_norma()
+        r = self.norma ** n
+        if abs(self.real)<1E-10:
+            theta = math.pi/2.0
+        else:
+            theta = math.atan(self.imaginario/self.real)
+        a.real = r * math.cos(n * theta)
+        a.imaginario = r * math.sin(n * theta)
+        return a 
+    
+    def multiplicacion(self, b):
+        real=self.real*b.real
+        reim=self.real*b.imaginario
+        mul=self.imaginario*b.real
+        multi=self.imaginario*b.imaginario
+        self.real=real - multi
+        self.imaginario=reim+mul
+        
+        
+        
